13c9a47f588e022ad5ec34359a1a3b7d200c2f59
Wellington Oliveira <well.oliveira.snt@gmail.com>
2017-09-08 04:53:43 -0400

    Modificando estrutura para componentes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component {
+export default class App extends Component {
     render() {
         return (
             <div className="App">
@@ -10,5 +10,3 @@ class App extends Component {
         );
     }
 }
-
-export default App;
